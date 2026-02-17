ec8453dc47e3943315b662d6b1dc1a16b7e3e897
Iris-Healy <kl3224zr@go.minnstate.edu>
2026-02-17 19:03:32 +0000

    Second commit fixed final card

--- TeenHangout/obj/Debug/net10.0/TeenHangout.AssemblyInfo.cs
+++ TeenHangout/obj/Debug/net10.0/TeenHangout.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeenHangout")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516bddc93ffe77aff4589347ea2ff52cd08f5fa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092359e8fe84b88b6d6cad06370269e4e77ead94")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeenHangout")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeenHangout")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
